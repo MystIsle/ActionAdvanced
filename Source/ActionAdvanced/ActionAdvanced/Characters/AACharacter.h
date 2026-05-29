362866546365fadf96b29df2ffeb8ad1e31f26d2
MystIsle <pelhav@gmail.com>
2026-05-29 11:19:56 +0900

    캐릭터 및 컨트롤러, 게임모드 기본 클래스 추가

---
+++ Source/ActionAdvanced/ActionAdvanced/Characters/AACharacter.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AACharacter.generated.h"
+
+UCLASS()
+class ACTIONADVANCED_API AAACharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAACharacter();
+
+public:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
---
+++ Source/ActionAdvanced/ActionAdvanced/Core/AAGameMode.h
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AAGameMode.generated.h"
+
+
+UCLASS()
+class ACTIONADVANCED_API AAAGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
